a30ef1e3de011c00b4fc184c3471835462f10f16
Jacky FAN <contact@jacky.fan>
2024-05-09 23:39:12 +0800

    ui: update terms-grid mobile layout

--- components/terms-grid/TermsGridItem.tsx
+++ components/terms-grid/TermsGridItem.tsx
@@ -4,12 +4,14 @@ export default function TermsGridItem({ img, heading, desc }) {
             <figure className="terms-grid-item__img-wrapper">
                 <img src={img} alt="" className="terms-grid-item__img" />
             </figure>
-            <h2 className="terms-grid-item__heading">
-                {heading}
-            </h2>
-            <p className="terms-grid-item__desc">
-                {desc}
-            </p>
+            <div className="terms-grid-item__text">
+                <h2 className="terms-grid-item__heading">
+                    {heading}
+                </h2>
+                <p className="terms-grid-item__desc">
+                    {desc}
+                </p>
+            </div>
         </div>
     );
 }
