e3298a762385da271eb310b00860677c262556ea
Jacky FAN <contact@jacky.fan>
2024-05-18 00:57:21 +0800

    ui: update navbar icon to link

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -11,11 +11,11 @@ export default function NavBar() {
         <header className="navbar">
             <div className="page-container">
                 <div className="navbar__inner">
-                    <div className="navbar__left">
+                    <a href="/" className="navbar__left">
                         <picture className="navbar__pic">
                             <img src="/assets/shared/desktop/logo-dark.png" alt="DESIGNO Logo" className="navbar__img" />
                         </picture>
-                    </div>
+                    </a>
                     <div className="navbar__right">
                         <Link href="/about" className="navbar__link navbar__link--desktop">Our Company</Link>
                         <Link href="/about" className="navbar__link navbar__link--desktop">Location</Link>
