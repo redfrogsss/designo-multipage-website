12ba23264553b8f48f726498ad2059dbe39c0216
Jacky Fan <contact@jacky.fan>
2024-05-21 22:50:58 +0800

    ui: update img-card responsive layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,8 +7,23 @@ export default function AboutPage () {
             <AboutBlock />
             <ImgCard 
                 img="/assets/about/desktop/image-world-class-talent.jpg"
+                tabletImg="/assets/about/tablet/image-world-class-talent.jpg"
+                mobileImg="/assets/about/mobile/image-world-class-talent.jpg"
                 title="World-class talent"
                 desc="<p>We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.</p><p><br></p><p>Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.</p>" 
+            />
+
+            <ImgCard 
+                img="/assets/about/desktop/image-real-deal.jpg"
+                tabletImg="/assets/about/tablet/image-real-deal.jpg"
+                mobileImg="/assets/about/mobile/image-real-deal.jpg"
+                title="The real deal"
+                desc="<p>As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.</p>
+                <p><br></p>
+                <p>
+                We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.
+                </p>" 
+                reverse={true}
             />
         </main>
     );
--- components/img-card/ImgCard.tsx
+++ components/img-card/ImgCard.tsx
@@ -1,10 +1,12 @@
-export default function ImgCard({ img, title, desc, reverse = false } : { img: string; title: string; desc: string, reverse?: boolean}) {
+export default function ImgCard({ img, tabletImg = img, mobileImg = img, title, desc, reverse = false }: { img: string; mobileImg?: string; tabletImg?: string; title: string; desc: string, reverse?: boolean }) {
     return (
-        <div className="img-card component-gap">
+        <div className={`img-card component-gap${reverse ? " img-card--reverse" : ""}`}>
             <div className="page-container">
                 <div className="img-card__inner">
                     <div className="img-card__left">
                         <picture className="img-card__pic">
+                            <source media="(max-width: 991px)" srcSet={tabletImg}></source>
+                            <source media="(max-width: 640px)" srcSet={mobileImg}></source>
                             <img src={img} alt={title} className="img-card__img" />
                         </picture>
                     </div>
