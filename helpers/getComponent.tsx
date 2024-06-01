f9d209f4a5962e05e73fe54ec03ebc3a101dd460
Jacky Fan <contact@jacky.fan>
2024-06-01 23:13:19 +0800

    fix: text wrapped into next line when part of sentence is formatted

--- helpers/getComponent.tsx
+++ helpers/getComponent.tsx
@@ -19,15 +19,20 @@ function getRichText(data: any) {
 
     data.forEach((item: any) => {
         if (item.type == "paragraph") {
+
+            let paraHTML = "";
+
             item.children.forEach((child: any) => {
                 if (child.type == "text") {
                     let text = child.text == "" ? "&nbsp;" : child.text;
                     if (child.bold == true) {
                         text = `<b>${text}</b>`;
                     }
-                    html += `<p>${text}</p>`;
+                    paraHTML += text;
                 }
             });
+            
+            html += `<p>${paraHTML}</p>`;
         }
         
         if (item.type == "heading") {
